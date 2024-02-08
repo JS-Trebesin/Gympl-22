f59f3dae1239351f98f06fa0ab49b21d6d41b434
JS <souhrada@trebesin.cz>
2024-02-08 09:56:56 +0100

    +7.2. javascript základy

---
+++ 2. ročník/02 Javascript/podmínky/podminky.js
@@ -0,0 +1,20 @@
+let cislo = -100
+
+if (cislo > 0) {
+    console.log("číslo je kladné")
+} else if (cislo < 0) {
+    console.log("číslo je záporné")
+} else {
+    console.log("číslo je 0")
+}
+
+let oblibenyClovek = window.prompt("Zadej jméno")
+
+if (oblibenyClovek === "Hvezdoň") {
+    console.log("To je frajer")
+} else {
+    console.log("Bůůů")
+}
+
+
+
---
+++ 2. ročník/02 Javascript/základy/zaklady.js
@@ -0,0 +1,18 @@
+let pozdrav = "Hello"
+
+console.log(pozdrav)
+
+pozdrav = "Ahoj"
+
+console.log(pozdrav)
+
+let jmeno = "Jarmila" // string = text
+
+console.log(pozdrav+jmeno)
+console.log(pozdrav + " " + jmeno)
+
+let cislo = 5 // int = celé číslo
+let druheCislo = 10 // float = desetinné číslo
+
+console.log(cislo+druheCislo)
+console.log(cislo+druheCislo, pozdrav)
